f3feab276ec09922eb85556dd0242756a612e212
straight28 <poem32@naver.com>
2022-03-20 23:29:32 +0900

    220320

---
+++ src/CSharpTet4/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest4
+{
+    //Observer Patten
+    class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if(Console.KeyAvailable == false) 
+                return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                InputKey();
+            }
+        }
+    }
+}
---
+++ src/CSharpTet4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpTest4
+{
+    internal class Program
+    {
+
+        static void OnInputTest()
+        {
+            Console.WriteLine("Input Received ! ");
+        }
+
+        enum ItemType
+        {
+            Weapon, 
+            Armor, 
+            Amulet, 
+            Ring
+        }
+
+        enum Rarity
+        {
+            Normal,
+            Uncommon,
+            Rare,
+        }
+
+        class Item
+        {
+            public ItemType ItemType;
+            public Rarity Rarity;
+        }
+
+
+        static List<Item> _items = new List<Item>();
+        static Item FindItem(Func<Item, bool> selector)
+        {
+            foreach (Item item in _items)
+            {
+                if(selector(item))
+                    return item; 
+            }
+            return null;
+        }
+
+
+        static void Main(string[] args)
+        {
+            InputManager inputManager = new InputManager();
+            inputManager.InputKey += OnInputTest;
+
+            while (true)
+            {
+                inputManager.Update();
+            }
+
+            _items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
+            _items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
+            _items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
+
+            // delegate 를 직접 선언하지 않아도, 이미 만들어진 것들
+            // => 반환 타입이 있을 경우 Func
+            // => 반환 타입이 없을 경우 Action
+
+            // Lambda : 일회용 함수를 만드는데 사용하는 문법이다.
+            // Anonymous Function : 무명 함수 / 익명 함수
+            Item item = FindItem((Item item) => { return item.ItemType == ItemType.Weapon; });
+        }
+    }
+}
